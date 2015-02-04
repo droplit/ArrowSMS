cd5b7d4f0b48ee969192d926c80f8e19a0af79c6
jpinz <julian.pinzer+development@gmail.com>
2015-02-03 22:04:58 -0500

    Maybe it works now? Please?

--- app/src/main/java/com/droplit/texcellent/MainActivity.java
+++ app/src/main/java/com/droplit/texcellent/MainActivity.java
@@ -1,172 +1,132 @@
 package com.droplit.texcellent;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.graphics.BitmapFactory;
-import android.os.Bundle;
 import android.provider.Telephony;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.text.TextUtils;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.ImageView;
-import com.klinker.android.logger.Log;
-import com.klinker.android.send_message.ApnUtils;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import com.klinker.android.send_message.Message;
+import com.klinker.android.send_message.Settings;
 import com.klinker.android.send_message.Transaction;
-import com.klinker.android.send_message.Utils;
 
 import java.util.ArrayList;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 
-    private Settings settings;
+    private LayoutInflater layoutInflater;
+    private ArrayList<String> texts = new ArrayList<String>(10);
 
-    private Button setDefaultAppButton;
-    private Button selectApns;
-    private EditText fromField;
-    private EditText toField;
-    private EditText messageField;
-    private ImageView imageToSend;
-    private Button sendButton;
-    private RecyclerView log;
+    Settings sendSettings = new Settings();
 
-    private LogAdapter logAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        initSettings();
-        initViews();
-        //initActions();
-        initLogging();
-    }
-
-    private void initSettings() {
-        settings = Settings.get(this);
-
-        if (TextUtils.isEmpty(settings.getMmsc())) {
-            initApns();
-        }
-    }
-
-    private void initApns() {
-        ApnUtils.initDefaultApns(this, new ApnUtils.OnApnFinishedListener() {
-            @Override
-            public void onFinished() {
-                settings = Settings.get(MainActivity.this, true);
-            }
-        });
-    }
-
-    private void initViews() {
-        setDefaultAppButton = (Button) findViewById(R.id.set_as_default);
-        selectApns = (Button) findViewById(R.id.apns);
-        fromField = (EditText) findViewById(R.id.from);
-        toField = (EditText) findViewById(R.id.to);
-        messageField = (EditText) findViewById(R.id.message);
-        imageToSend = (ImageView) findViewById(R.id.image);
-        sendButton = (Button) findViewById(R.id.send);
-        log = (RecyclerView) findViewById(R.id.log);
-    }
-
-    private void initActions() {
-        if (Utils.isDefaultSmsApp(this)) {
-            setDefaultAppButton.setVisibility(View.GONE);
-        } else {
-            setDefaultAppButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    setDefaultSmsApp();
-                }
-            });
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
         }
 
-        selectApns.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                initApns();
-            }
-        });
+        sendSettings.setMmsc("http://mms.vtext.com/servlets/mms");
+        sendSettings.setProxy("");
+        sendSettings.setPort("80");
+        sendSettings.setGroup(true);
+        sendSettings.setDeliveryReports(true);
+        sendSettings.setSplit(true);
+        sendSettings.setSplitCounter(true);
+        sendSettings.setStripUnicode(false);
+        sendSettings.setSignature("");
+        sendSettings.setSendLongAsMms(true);
+        sendSettings.setSendLongAsMmsAfter(3);
+        sendSettings.setAccount("julian.pinzer@gmail.com");
+        sendSettings.setRnrSe(null);
 
-        fromField.setText(Utils.getMyPhoneNumber(this));
-        toField.setText(Utils.getMyPhoneNumber(this));
-
-        imageToSend.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                toggleSendImage();
-            }
-        });
-
-        sendButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                sendMessage();
-            }
-        });
-
-        log.setHasFixedSize(false);
-        log.setLayoutManager(new LinearLayoutManager(this));
-        logAdapter = new LogAdapter(new ArrayList<String>());
-        log.setAdapter(logAdapter);
     }
 
-    private void initLogging() {
-        Log.setDebug(true);
-        Log.setPath("messenger_log.txt");
-        /*Log.setLogListener(new OnLogListener() {
-            @Override
-            public void onLogged(String tag, String message) {
-                logAdapter.addItem(tag + ": " + message);
-            }
-        });*/
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
-    private void setDefaultSmsApp() {
-        setDefaultAppButton.setVisibility(View.GONE);
-        Intent intent =
-                new Intent(Telephony.Sms.Intents.ACTION_CHANGE_DEFAULT);
-        intent.putExtra(Telephony.Sms.Intents.EXTRA_PACKAGE_NAME,
-                getPackageName());
-        startActivity(intent);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        if (id == R.id.action_default) {
+            Intent intent =
+                    new Intent(Telephony.Sms.Intents.ACTION_CHANGE_DEFAULT);
+            intent.putExtra(Telephony.Sms.Intents.EXTRA_PACKAGE_NAME,
+                    getPackageName());
+            startActivity(intent);
+
+            Toast.makeText(getApplicationContext(),"Set as Default texting app.", Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
-    private void toggleSendImage() {
-        if (imageToSend.isEnabled()) {
-            imageToSend.setEnabled(false);
-            imageToSend.setAlpha(0.3f);
-        } else {
-            imageToSend.setEnabled(true);
-            imageToSend.setAlpha(1.0f);
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.messaging, container, false);
+            return rootView;
         }
     }
+    public void send(View view) {
 
-    public void sendMessage() {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                com.klinker.android.send_message.Settings sendSettings = new com.klinker.android.send_message.Settings();
-                sendSettings.setMmsc(settings.getMmsc());
-                sendSettings.setProxy(settings.getMmsProxy());
-                sendSettings.setPort(settings.getMmsPort());
+        EditText messageField = (EditText) findViewById(R.id.messageBodyField);
+        String text = messageField.getText().toString();
+        ListView listView = (ListView) findViewById(R.id.listMessages);
 
-                Transaction transaction = new Transaction(MainActivity.this, sendSettings);
+        ViewGroup parent = (ViewGroup) findViewById(R.id.mainView);
 
-                Message message = new Message(messageField.getText().toString(), toField.getText().toString());
-                message.setType(Message.TYPE_SMSMMS);
+        View viewR = LayoutInflater.from(this).inflate(R.layout.message_right, null);
+        //View viewL = LayoutInflater.from(this).inflate(R.layout.message_left, null);
+        parent.addView(viewR);
+        TextView txtMessage = (TextView) findViewById(R.id.txtMessage);
+        txtMessage.setText(text);
 
-                if (imageToSend.isEnabled()) {
-                    message.setImage(BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher));
-                }
+        Transaction transaction = new Transaction(MainActivity.this, sendSettings);
 
-                transaction.sendNewMessage(message, Transaction.NO_THREAD_ID);
-            }
-        }).start();
+        Message message = new Message(messageField.getText().toString(), "9785023741");
+        message.setType(Message.TYPE_SMSMMS);
+
+        transaction.sendNewMessage(message, Transaction.NO_THREAD_ID);
+        Log.d("TEXT", "Sent");
     }
-
 }
